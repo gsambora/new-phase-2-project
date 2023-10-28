d97bff7de038bd09ceaa5a1cd82673d1a404c0ce
Hana Sambora <grace.sambora@gmail.com>
2023-10-27 21:30:31 -0500

    Added text to about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,13 @@ function About(){
     return(
         <div>
             <h1>About</h1>
+            <body>
+                Welcome to my project for phase 2 at Flatiron School! My name is Hana, and
+                I made this since I need to track my own meals. <br /> <br />                
+                You can use this app to track your meal intake during the day.
+                It's recommended to have breakfast, lunch, and dinner with a snack in 
+                between each meal or three snacks total in a day. Remember to drink water too!
+            </body>
         </div>
     )
 }
