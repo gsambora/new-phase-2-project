d4c3534c16f3f9d17656c180ed6d430dfcc8ce05
Hana Sambora <grace.sambora@gmail.com>
2023-10-29 18:22:43 -0500

    Added map of meal array to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
+import MealCard from "./MealCard.js";
 
 function Home({meals}){
-    console.log(meals);
-    
+    console.log("All Meals: "+meals);
+
     return(
         <div>
             <h1>Home</h1>
+
+            <ul className="allMealCards">{
+                meals.map((meal)=>{
+                    console.log(meal)
+                    return(<MealCard key={meal.id} id={meal.id} mealtime={meal.mealtime}
+                        food={meal.food} image={meal.image} liked={food.liked} />)
+                })
+            }</ul>
         </div>
     )
 }
