b0d5e4d873b559224d5b676e20cfc2cec783f783
Hana Sambora <grace.sambora@gmail.com>
2023-10-29 18:42:16 -0500

    Styling for mealcards

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -4,11 +4,11 @@ function MealCard({id, day, mealtime, food,
                     image="https://preview.redd.it/7zdwq4rv6ma91.png?width=640&crop=smart&auto=webp&s=3abbfac771b1cbdcd1a027b83d3b94d656f00a66", 
                     liked="false"}) {
     return(
-        <li id={"meal"+id} className="card">
-            <img src={image} alt={"picture of meal "+id} />
+        <div id={"meal"+id} className="card">
             <h4>{day +" "+ mealtime+ ": "+ food}</h4>
-            <button id={"btn"+id}>Liked</button>
-        </li>
+            <img src={image} alt={"picture of meal "+id} />
+            <button className = "btn" id={"btn"+id}>Liked</button>
+        </div>
     )
 }
 
