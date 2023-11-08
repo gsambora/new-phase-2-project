04335c8ca39d958b9380a300824032c54adeb183
Hana <grace.sambora@gmail.com>
2023-11-07 18:17:09 -0600

    Pressing like button updates database

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -4,11 +4,21 @@ function MealCard({id, day, mealtime, food,
                     image="https://preview.redd.it/7zdwq4rv6ma91.png?width=640&crop=smart&auto=webp&s=3abbfac771b1cbdcd1a027b83d3b94d656f00a66", 
                     liked="false"}) {
 
-    const [likeHeart, setLike] = useState(false);
+    const [likeHeart, setLike] = useState(liked);
 
     function handleLikeClick(){
         console.log("clicked!")
-        setLike(!likeHeart);
+        const newLikeStatus = !likeHeart;
+        setLike(newLikeStatus);
+        const url = "http://localhost:3000/meals/"+id;
+        fetch(url, {
+            method:"PATCH",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({liked: newLikeStatus})
+
+        })
     }
 
     return(
